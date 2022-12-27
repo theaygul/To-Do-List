b31d91106f596ec7a60025cdb247ed1e1400af4c
theaygul <114475217+theaygul@users.noreply.github.com>
2022-12-27 13:52:05 +0400

    Add files via upload

---
+++ src/NewTodoForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import './NewTodoForm.css'
+
+class NewTodoForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      task: '',
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleSubmit(e) {
+    e.preventDefault()
+    this.props.createTodo({ ...this.state, id: uuidv4(), completed: false })
+    this.setState({ task: '' })
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+  render() {
+    return (
+      <form className='NewTodoForm' onSubmit={this.handleSubmit}>
+        <label htmlFor='task'>Planlar:</label>
+        <input
+          id='task'
+          type='text'
+          name='task'
+          placeholder='Əlavələr et!'
+          value={this.state.task}
+          onChange={this.handleChange}
+        ></input>
+        <button>
+          <i className='fa-solid fa-plus'></i>
+        </button>
+      </form>
+    )
+  }
+}
+
+export default NewTodoForm
